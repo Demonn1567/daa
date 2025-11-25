295b3c4386d49c16c1cd961190a99d1065c52698
Krish Sharma <krishsharmacool3@gmail.com>
2025-11-25 13:08:26 +0530

    nqueens

--- activitySelection.cpp
+++ activitySelection.cpp
@@ -2,5 +2,60 @@
 using namespace std;
 
 bool comp(const pair<int,int>& a, const pair<int,int>& b) {
-    return a.second < b.second();
+    return a.second < b.second;
+}
+
+vector<pair<int,int>> ActSelect(const vector<pair<int,int>> & activities) {
+    int n = (int)activities.size();
+    vector<pair<int,int>> A;
+
+    if(n==0) {
+        return A;
+    }
+    A.push_back(activities[0]);
+
+    int k = 0;
+    for(int m =1;m<n;m++) {
+        if(activities[m].first>=activities[k].second) {
+            A.push_back(activities[m]);
+            k=m;
+        }
+    }
+    return A;
+}
+
+int main() {
+    vector<pair<int,int>> activities = {
+        {1, 4},
+        {3, 5},
+        {0, 6},
+        {5, 7},
+        {3, 9},
+        {5, 9},
+        {6, 10},
+        {8, 11},
+        {8, 12},
+        {2, 14},
+        {12, 16}
+    };
+
+    sort(activities.begin(), activities.end(), comp);
+
+    vector<pair<int,int>> selected =ActSelect(activities);
+
+    cout << "Selected activities (start, finish):\n";
+    for (size_t i = 0; i < selected.size(); i++) {
+        cout << "(" << selected[i].first << ", " << selected[i].second << ")\n";
+    }
+
+    cout << "Maximum number of non-overlapping activities = "
+         << selected.size() << endl;
+
+
+    
+
+
+
+
+    return 0;
 }
---
+++ nqueens.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void printarray(int a[], int n) {
+    for(int i =0;i<n;i++) {
+        cout<<a[i]<<" ";
+    }
+    cout<<"\n";
+}
+
+int issafe(int result[], int x2, int y2) {
+    for(int i =0;i<x2;i++) {
+        
+        //same column conflict
+        if(result[i]==y2) {
+            return 0;
+        } 
+        if(abs(i-x2)==abs(result[i]-y2)) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void placequeens(int result[], int x, int size) {
+    if(x==size) {
+        cout<<"Solution found : ";
+        printarray(result, size);
+        return;
+    }
+    for(int i =0;i<size;i++) {
+        if(issafe(result, x,i)==1) {
+            result[x] =i;
+            placequeens(result, x+1, size);
+        }
+    }
+
+}
+
+int main() {
+    int size= 4;
+    int result[10];
+    placequeens(result,0,size);
+    
+    
+    
+    return 0;
+}
