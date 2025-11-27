5f9c482f7f3c8a80a5740c92a4b9b5bc168c4351
Krish Sharma <krishsharmacool3@gmail.com>
2025-11-28 00:12:09 +0530

    final-jobSequencing

--- jobSequencing.cpp
+++ jobSequencing.cpp
@@ -2,73 +2,57 @@
 using namespace std;
 
 int main() {
-    // jobs[i][0] = profit
-    // jobs[i][1] = deadline
-    int jobs[7][2] = {
-        {35, 3},
-        {30, 4},
-        {25, 4},
-        {20, 2},
-        {15, 3},
-        {12, 1},
-        {5,  2}
+    int arr[][2] = {
+        {35,3},
+        {30,4},
+        {25,4},
+        {20,2},
+        {15,3},
+        {12,1},
+        {5,2}
     };
-
-    int n = 7;
-
-    // -------------------------------
-    // Step 1: Bubble sort by profit (descending)
-    // -------------------------------
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (jobs[j][0] < jobs[j + 1][0]) {
-                // swap both profit and deadline
-                int tempProfit = jobs[j][0];
-                int tempDeadline = jobs[j][1];
-
-                jobs[j][0] = jobs[j + 1][0];
-                jobs[j][1] = jobs[j + 1][1];
-
-                jobs[j + 1][0] = tempProfit;
-                jobs[j + 1][1] = tempDeadline;
+    int n =7;
+    
+    for(int i =0;i<n-1;i++) {
+        for(int j =i+1;j<n;j++) {
+            if(arr[i][0]<arr[j][0]) {
+                int profit = arr[i][0];
+                int deadline = arr[i][1];
+                
+                arr[i][0] = arr[j][0];
+                arr[i][1] = arr[j][1];
+                
+                arr[j][0] = profit;
+                arr[j][1] = deadline;
             }
-        }
+        }    
     }
-
-    // -------------------------------
-    // Step 2: Find the max deadline
-    // -------------------------------
     int last = 0;
-    for (int i = 0; i < n; i++) {
-        if (jobs[i][1] > last) {
-            last = jobs[i][1];
+    for(int i =0;i<n;i++) {
+        if(arr[i][1]>last) {
+            last = arr[i][1];
         }
     }
-
-    // slots array of size = last + 1
-    // slot[t] = 1 means occupied
-    int slots[100] = {0}; // enough size
-
+    
+    int slots[last+1] = {0};
     int totalProfit = 0;
-
-    // -------------------------------
-    // Step 3: Job Sequencing Logic
-    // -------------------------------
-    for (int i = 0; i < n; i++) {
-        int profit = jobs[i][0];
-        int deadline = jobs[i][1];
-
-        // try placing this job at the latest slot <= deadline
-        for (int t = deadline; t >= 1; t--) {
-            if (slots[t] == 0) {
-                slots[t] = 1;
-                totalProfit += profit;
+    for(int i=0;i<n;i++) {
+        int p = arr[i][0];
+        int d = arr[i][1];
+        
+        for(int t = d;t>=1;t--) {
+            if(slots[t]==0) {
+                slots[t] =1;
+                totalProfit+=p;
                 break;
             }
         }
+        
     }
-
-    cout << "Total Profit = " << totalProfit << endl;
-
+    cout<<"Total Profit="<<totalProfit<<endl;
+    
     return 0;
+    
+    
+    
 }
